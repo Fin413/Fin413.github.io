72be669d1ae007efd3e9697ddf3566198c41a575
Fin413 <64548196+Fin413@users.noreply.github.com>
2021-07-14 22:28:38 -0600

    Update main.js

--- noiseCancelling/main.js
+++ noiseCancelling/main.js
@@ -1,17 +1,18 @@
 var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
 var dropdownInit = true;
 
+
 function showDropdown(){
-    // dropdownInit = false;
-    document.getElementsByClassName("dropdown btn")[0].focus();
-    console.log("hi");
-    // setTimeout(()=>{
-    //     dropdownInit = true;
-    // }, 100);
+    dropdownInit = false;
+    dropdownContent.classList.add("showDropdown");
+
+    setTimeout(()=>{
+        dropdownInit = true;
+    }, 100);
 }
 
-// document.onclick = () => {
-//     if(dropdownInit){
-//         dropdownContent.style.display = "none";
-//     }
-// }
+document.onclick = () => {
+    if(dropdownInit){
+        dropdownContent.classList.remove("showDropdown");
+    }
+}
