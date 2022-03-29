b7c3861ae0a59b47b6784d5e8ead95dad0514f49
Fin413 <64548196+Fin413@users.noreply.github.com>
2022-03-28 23:39:40 -0600

    Add files via upload

---
+++ crosslord/mobileTouch.js
@@ -0,0 +1,93 @@
+var evCache = new Array();
+var prevDiff = -1;
+init();
+
+function init() {
+    // Install event handlers for the pointer target
+    var el=document.getElementById("crosswordContainer");
+    el.onpointerdown = pointerdown_handler;
+    el.onpointermove = pointermove_handler;
+
+    // Use same handler for pointer{up,cancel,out,leave} events since
+    // the semantics for these events - in this app - are the same.
+    el.onpointerup = pointerup_handler;
+    el.onpointercancel = pointerup_handler;
+    el.onpointerout = pointerup_handler;
+    el.onpointerleave = pointerup_handler;
+}
+
+function pointerdown_handler(ev) {
+    // The pointerdown event signals the start of a touch interaction.
+    // This event is cached to support 2-finger gestures
+    evCache.push(ev);
+    // console.console.log("pointerDown");
+}
+
+function pointermove_handler(ev) {
+    // This function implements a 2-pointer horizontal pinch/zoom gesture.
+    //
+    // If the distance between the two pointers has increased (zoom in),
+    // the target element's background is changed to "pink" and if the
+    // distance is decreasing (zoom out), the color is changed to "lightblue".
+    //
+    // This function sets the target element's border to "dashed" to visually
+    // indicate the pointer's target received a move event.
+    // console.log("pointerMove", ev);
+    // ev.target.style.border = "dashed";
+
+    // Find this event in the cache and update its record with this event
+    for (var i = 0; i < evCache.length; i++) {
+        if (ev.pointerId == evCache[i].pointerId) {
+            evCache[i] = ev;
+            break;
+        }
+    }
+
+    // If two pointers are down, check for pinch gestures
+    if (evCache.length == 2) {
+        // Calculate the distance between the two pointers
+        var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+
+        if (prevDiff > 0) {
+            if (curDiff > prevDiff) {
+                // The distance between the two pointers has increased
+                // console.log("Pinch moving OUT -> Zoom in", ev);
+                // ev.target.style.background = "pink";
+                resize(5);
+            }
+            if (curDiff < prevDiff) {
+                // The distance between the two pointers has decreased
+                // console.log("Pinch moving IN -> Zoom out",ev);
+                // ev.target.style.background = "lightblue";
+                resize(-5);
+            }
+        }
+
+        // Cache the distance for the next move event
+        prevDiff = curDiff;
+    }
+}
+
+function pointerup_handler(ev) {
+    // console.log(ev.type, ev);
+    // Remove this pointer from the cache and reset the target's
+    // background and border
+    remove_event(ev);
+    // ev.target.style.background = "white";
+    // ev.target.style.border = "1px solid black";
+  
+    // If the number of pointers down is less than two then reset diff tracker
+    if (evCache.length < 2) {
+      prevDiff = -1;
+    }
+  }
+
+  function remove_event(ev) {
+    // Remove this event from the target's cache
+    for (var i = 0; i < evCache.length; i++) {
+      if (evCache[i].pointerId == ev.pointerId) {
+        evCache.splice(i, 1);
+        break;
+      }
+    }
+   }
