1c135f103824f99f365754fb385cbb35e97b573a
Fin413 <64548196+Fin413@users.noreply.github.com>
2022-04-03 20:29:16 -0600

    Add files via upload

---
+++ test/dragControls.js
@@ -0,0 +1,70 @@
+var startingCoord;
+const crossword = document.getElementById("crossword");
+var crosswordDimensions = crossword.getBoundingClientRect();
+var scale = 1;
+
+var xPos = ((document.getElementById("crosswordContainer").offsetWidth / 2) / scale) - (document.getElementById("crossword").offsetWidth / 2);
+var yPos =((document.getElementById("crosswordContainer").offsetHeight / 2) / scale) - (document.getElementById("crossword").offsetHeight / 2);
+crossword.style.transform = "scale(" + scale + ") " + "translate(" + xPos + "px," + yPos + "px)";
+
+document.getElementById("crosswordContainer").onwheel = e => {
+    e.preventDefault();
+
+    if(e.deltaY > 0){
+        resize(5, e);
+    }else{
+        resize(-5, e);
+    }
+
+    return false;
+}
+
+crossword.onpointerdown = e => {
+    startingCoord = [e.clientX, e.clientY];
+}
+
+window.onpointerup = () => {
+    startingCoord = undefined;
+}
+
+crossword.onpointermove = e => {
+    if(startingCoord != undefined){
+        moveCrossword(e.clientX, e.clientY, startingCoord[0] - e.clientX, startingCoord[1] - e.clientY);
+    }
+}
+
+function moveCrossword(mouseX, mouseY, x, y){
+    let containerWidth = document.getElementById("crosswordContainer").offsetWidth;
+    let containerHeight = document.getElementById("crosswordContainer").offsetHeight;
+
+    let rightWall = ((crossword.offsetWidth) - (((Math.abs(xPos) + (containerWidth / scale)))));
+    let bottomWall = ((crossword.offsetHeight) - (((Math.abs(yPos) + (containerHeight / scale)))));
+    
+    if((xPos < 0 && x < 0) || (rightWall > 0 && x > 0)){
+        xPos -= x * 1 / scale;        
+    }
+    if((yPos < 0 && y < 0) ||  (bottomWall > 0 && y > 0)){
+        yPos -= y * 1 / scale;
+    }
+
+    // let originX = ((document.getElementById("crosswordContainer").offsetWidth / 2) / scale) - (document.getElementById("crossword").offsetWidth / 2);
+    // const currentCenterX = Math.abs(originX) + (xPos);
+    // console.log("origin", originX)
+    // console.log("dist form origin", currentCenterX);
+    let originX = ((document.getElementById("crosswordContainer").offsetWidth / 2) / scale) - (document.getElementById("crossword").offsetWidth / 2);
+    let originY =((document.getElementById("crosswordContainer").offsetHeight / 2) / scale) - (document.getElementById("crossword").offsetHeight / 2);
+    const xDistFromCenter = Math.abs(originX * scale) + (xPos);
+    const yDistFromCenter = Math.abs(originY * scale) + (yPos)
+    // console.log(xDistFromCenter - (crossword.offsetWidth / 2) + (document.getElementById("crosswordContainer").offsetWidth / 2), yDistFromCenter);
+
+    crossword.style.transform = "scale(" + scale + ") " + "translate(" + xPos + "px," + yPos + "px)";
+    startingCoord = [mouseX, mouseY];
+}
+
+function setX(x){
+    xPos = x;
+    const xDistFromCenter = Math.abs(originX * scale) + (xPos);
+    const pos = (xDistFromCenter - (crossword.offsetWidth / 2) + (document.getElementById("crosswordContainer").offsetWidth / 2));
+    xPos = pos;
+    crossword.style.transform = "scale(" + scale + ") " + "translate(" + xPos + "px," + yPos + "px)";
+}
---
+++ test/mobileTouch.js
@@ -0,0 +1,69 @@
+var evCache = new Array();
+var prevDiff = -1;
+
+function init() {
+    // Install event handlers for the pointer target
+    var el = document.getElementById("crosswordContainer");
+    el.onpointerdown = pointerdown_handler;
+    el.onpointermove = pointermove_handler;
+
+    // Use same handler for pointer{up,cancel,out,leave} events since
+    // the semantics for these events - in this app - are the same.
+    el.onpointerup = pointerup_handler;
+    el.onpointercancel = pointerup_handler;
+    el.onpointerout = pointerup_handler;
+    el.onpointerleave = pointerup_handler;
+}
+
+function pointerdown_handler(ev) {
+    // The pointerdown event signals the start of a touch interaction.
+    // This event is cached to support 2-finger gestures
+    evCache.push(ev);
+}
+
+function pointermove_handler(ev) {
+    for (var i = 0; i < evCache.length; i++) {
+        if (ev.pointerId == evCache[i].pointerId) {
+            evCache[i] = ev;
+            break;
+        }
+    }
+
+    if (evCache.length == 2) {
+        // Calculate the distance between the two pointers
+        var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+
+        if (prevDiff > 0) {
+            if (curDiff > prevDiff) {
+                // The distance between the two pointers has increased
+                resize(1);
+            }
+            if (curDiff < prevDiff) {
+                // The distance between the two pointers has decreased
+                resize(-1);
+            }
+        }
+
+        // Cache the distance for the next move event
+        prevDiff = curDiff;
+    }
+}
+
+function pointerup_handler(ev) {
+    remove_event(ev);
+  
+    // If the number of pointers down is less than two then reset diff tracker
+    if (evCache.length < 2) {
+        prevDiff = -1;
+    }
+}
+
+function remove_event(ev) {
+    // Remove this event from the target's cache
+    for (var i = 0; i < evCache.length; i++) {
+        if (evCache[i].pointerId == ev.pointerId) {
+            evCache.splice(i, 1);
+            break;
+        }
+    }
+}
